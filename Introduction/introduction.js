f8bb8f1067febff5420daefaa43c7f337e7b7e76
Stan Goldyn <sagoldyn@gmail.com>
2018-12-16 04:50:31 -0500

    provided code for the versions of the function

--- Introduction/introduction.js
+++ Introduction/introduction.js
@@ -1,6 +1,23 @@
-function introduction(first, last){
-    let introTxt = 'Hi, my name is ';
+function introduction(first, last) {
+    const introTxt = 'Hi, my name is ';
+    
     return introTxt.concat(first, ' ', last);
 }
 
+
+function introduction2(first, last) {
+    const introTxt = 'Hi, my name is ' .concat(first, " ", last, ".");
+    
+    return introTxt;
+}
+
+
+function introduction3(first, last) {
+    const introTxt = 'Hi, my name is ${first} ${last}.';
+    
+    return introTxt;
+}
+
 console.log(introduction('Stan', 'Goldyn'))
+console.log(introduction2('Stan', 'Goldyn'))
+console.log(introduction3('Stan', 'Goldyn'))
