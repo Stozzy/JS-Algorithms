b796a98513584496df377089c849186daebf1a82
Stan <40995283+Stozzy@users.noreply.github.com>
2018-12-20 05:32:45 -0500

    Updated code using split(), for loop and if stmt
    Based on solution video of Udemy course.  I was over thinking this....

--- Longest Word/longestWord.js
+++ Longest Word/longestWord.js
@@ -1,17 +1,33 @@
-function longestWordInString (str) {
-    let arraySize = str.legth - 1;
-    let temp = str.split('');
-    let wordLengthArray = [];
-    let count = 0;
+// function longestWordInString (str) {
+//     let arraySize = str.legth - 1;
+//     let temp = str.split('');
+//     let wordLengthArray = [];
+//     let count = 0;
     
-    temp.forEach((element) => {
-        if (element != ' ') {
-            count++;
-        } else {
-            wordLengthArray.push(count);
-            count = 0;
+//     temp.forEach((element) => {
+//         if (element != ' ') {
+//             count++;
+//         } else {
+//             wordLengthArray.push(count);
+//             count = 0;
+//         }
+//     });
+//     return Math.max.apply(null, wordLengthArray);
+// }
+
+// After watching a solution I realized I did not fully understand split() method completely:
+
+function longestWordInString (str) {
+    let longestLength = 0;
+    let strArray = str.split(' ');
+
+    for (let i = 0; i < strArray.length; i++) {
+        if (longestLength < strArray[i].length) {
+            longestLength = strArray[i].length;
         }
-    });
-    return Math.max.apply(null, wordLengthArray);
+    // Tenary operators to elminate the if stmt as it's a simple conditional check
+    }
+    return longestLength;
 }
-console.log(longestWordInString('Good bye son'))
+
+console.log(longestWordInString('All of your base are belong to us'));
