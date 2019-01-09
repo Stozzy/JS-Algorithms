7ddea68cf564651c9daeb62f653e5368d03c32d9
Stan <40995283+Stozzy@users.noreply.github.com>
2019-01-09 06:09:44 -0500

    Updated
    This was on FCC so I did the challenge and updated the js file on GitHub.

--- Palindrome/palindrome.js
+++ Palindrome/palindrome.js
@@ -1,23 +1,12 @@
-function palindrome (phrase) {
-    initialPhrase = phrase.toLowerCase().replace(/[.@$%^&*(/)\,_#!?/]| /gi, '');
-    reversePhrase = initialPhrase.split('').reverse().join('');
-function palindrome (phrase) {
->>>>>>> 955d3c4de582a54cb67517c58b6ff977a0317f0e
-    initialPhrase = phrase.toLowerCase().replace(/[.@$%^&*(/)\,_#!?/]| /gi, '');
-    reversePhrase = initialPhrase.split('').reverse().join('');
+function palindrome(str) {
+    const initialPhrase = str.toLowerCase().replace(/[^A-Z0-9]|/gi, '');
+    const reversePhrase = initialPhrase.split('').reverse().join('');
 
-    return initialPhrase === reversePhrase
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log(isPalindrome('B/O\B'));
-=======
-console.log(palindrome('B/O\B'));
->>>>>>> 955d3c4de582a54cb67517c58b6ff977a0317f0e
-=======
-console.log(palindrome('B/O\B'));
->>>>>>> 347a500589361fba251fa5e982dae74974b2217c
-=======
-console.log(palindrome('B/O\B'));
->>>>>>> 347a500589361fba251fa5e982dae74974b2217c
+    return initialPhrase === reversePhrase;
+};
+
+palindrome("eye");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("0_0 (: /-\ :) 0-0");
+palindrome("five|\_/|four") ;
+palindrome("not a palindrome");
