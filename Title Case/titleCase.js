22b439e687781c0fe0c55d758b2108617a3cb6ba
Stan <40995283+Stozzy@users.noreply.github.com>
2019-02-15 05:56:56 -0500

    uploaded alternate solution

--- Title Case/titleCase.js
+++ Title Case/titleCase.js
@@ -16,6 +16,16 @@
 //    return strOutput;
 // }
 
+// THE SNIPPET BELOW IS INSPIRED BY FCC
+// function titleCase(str) {
+//   let convertToArray = str.toLowerCase().split(" ");
+//   let result = convertToArray.map(function(val) {
+//       return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+//   });
+//   return result.join(" ");
+// }
+// titleCase("all of YOUR Base are BELong tO us");
+
 
 function titleCase (str) {
     let titleCaseArray = str.split(' ');
